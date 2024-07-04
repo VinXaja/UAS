d29617ef3c4aff046181d55c6be2915b758fb12f
VinXaja <156817237+VinXaja@users.noreply.github.com>
2024-07-04 16:17:46 +0700

    Update

--- UAS/UAS.py
+++ UAS/UAS.py
@@ -1,4 +1,5 @@
 from tabulate import tabulate
+import math
 
 class Express:
     def __init__(self):
@@ -10,6 +11,19 @@ class Express:
         "Freeham": {"Cowstone": 2, "Donningpool": 3, "Wingtown": 5},
         "Bingborough": {"Donningpool": 2, "Highbrook":1},
         "Donningpool": {"Wingtown": 4,"Highbrook": 5, "Old Mere": 2}
+        }
+        
+        self.FullRoute = {
+            "Minstowe" : {"Cowstone": 3},
+            "Oldcastle" : {"New North": 5, "Freeham": 2},
+            "Cowstone": {"New North": 4, "Bingborough": 6, "Donningpool": 7, "HighBrook": 5, "Minstowe": 3, "Freeham": 2},
+            "New North": {"Bingborough": 3, "Donningpool": 6, "Wington": 4, "Highbrook": 2, "Oldcastle": 5, "Cowstone": 4},
+            "Freeham": {"Cowstone": 2, "Donningpool": 3, "Wingtown": 5, "Oldcastle": 2},
+            "Bingborough": {"Donningpool": 2, "Highbrook":1, "Cowstone": 6, "New North": 3},
+            "Donningpool": {"Wingtown": 4,"Highbrook": 5, "Old Mere": 2, "Cowstone": 7, "New North": 6, "Freeham": 3, "Bingborough": 2},
+            "Highbrook": {"Cowstone": 5, "New North": 2, "Bingborough":1, "Donningpool": 5 },
+            "Wingtown": {"New North": 4, "Freeham": 5, "Donningpool":4},
+            "Old Mere": {"Donningpool": 2}
         }
         
         self.Train = {
@@ -100,7 +114,7 @@ class Express:
                     print()
                 
                 choice = input("Can Your Items be Divided Into Parts ? (Yes / No) : ").lower()
-                print("We Recommend You To Bring")
+                
                 if choice == "no":
                     item.sort(key = lambda x : (x[2], x[1]))
                     for i in range(n):
@@ -109,8 +123,10 @@ class Express:
                             total_weight += item[i][1]
                             item_take.append(item[i][0])
                     if len(item_take) != len(item):
+                        print("We Recommend You To Bring :")
                         for i in range(len(item_take)):
                             print(f"{i + 1}. {item_take[i]}")
+                        print(f"With Total Weight : {total_weight} Kg")
                         
                 elif choice == "yes":
                     item.sort(key = lambda x : (-(x[1] / x[2]), x[2]))
@@ -118,18 +134,20 @@ class Express:
                         if weight_current >= item[i][1]:
                             weight_current -= item[i][1]
                             total_weight += item[i][1]
-                            item_take.append((item[i][0], "WHOLE"))
+                            item_take.append((item[i][0], "Whole"))
                         elif weight_current < item[i][1] and weight_current != 0:
-                            item_take.append((item[i][0], f"{weight_current}/{item[i][1]}"))
+                            fpb = math.gcd(weight_current, item[i][1])
+                            item_take.append((item[i][0], f"{int(weight_current / fpb)}/{int(item[i][1] / fpb)}"))
                             total_weight += weight_current
                             weight_current -= weight_current
                             
-                    if len(item_take) != len(item):
-                        for i in range(len(item_take)):
-                            print(f"{i + 1}. {item_take[i][0]} ({item_take[i][1]})")
-                
-                print(f"With Total Weight : {total_weight}")
-                if len(item) == len(item_take): print(f"You Can Carry All of Your Items")
+                    print("We Recommend You To Bring :")
+                    for i in range(len(item_take)):
+                        print(f"{i + 1}. {item_take[i][0]} ({item_take[i][1]} Parts)")
+                    print(f"With Total Weight : {total_weight} Kg")
+                    
+                if len(item_take) == 0: print("You Can't Carry All of Your Belongings")
+                elif len(item) == len(item_take): print(f"You Can Carry All of Your Items")
                 else: print("You Can Use Our Recommendation or Choose Another Class to Carry More")
                 loop = input("\nTry Another Class ? (Yes / No) : ").lower()
                 if loop == "no": break
